6c8e39cbe700580c55b45dd42baa775f6417e5f7
Morgan <morgan.gadiou@gmail.com>
2019-03-18 11:57:20 +0100

    Commit SIKULI

---
+++ DeclarationScript/script/script.js
@@ -0,0 +1,36 @@
+//Permet de vérifier si une varible est bien déclarée
+//et si elle n'est pas présenté plusieurs fois
+
+'use-strict'
+
+
+//déclaration d'une variable
+var nom = "var1";
+let prenom = "Squand";
+const age = 28;
+
+//Type de données
+//type string ou chaine de caractères :"" ou ''
+//type Number ou nombre
+// type boolean 0 ou 1 / true ou false
+//type Null
+
+nom ="Gadiou";
+prenom ="Morgan";
+
+
+console.log(nom);
+console.log(prenom);
+
+
+let u=null
+let uu;
+let nn=Nan;   //Not a Number
+
+console log(typeof(u));
+console log(typeof(uu));
+console log(typeof(nn));
+
+
+
+let listPrenom=["François","Pierre","Paul"]
