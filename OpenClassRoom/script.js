74c7cf06171e6ae921c64e14cac05d7cb5273948
Squand <morgan.gadiou@gmail.com>
2019-03-25 08:51:33 +0100

    commit OpenClass

---
+++ OpenClassRoom/script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const nbTours = 10
+
+console.log("Le manège démarre");
+
+// Ajoutz votre code ici
+     {
+  console.log(`C'est le tour numéro ${i+1}`);
+}
+console.log("Le manège s'arrête");
+
+
